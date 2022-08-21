d190e55fba741e004a7653fee2c2447d79004e9f
Ty Corcoran <45568306+tcorcor1@users.noreply.github.com>
2022-08-21 14:04:53 -0400

    API: remove teams/{guid} action

--- src/WebAPI/Controllers/TeamsController.cs
+++ src/WebAPI/Controllers/TeamsController.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using NhlStatsCrm.Domain.Entities.Crm;
 using NhlStatsCrm.Application.Features.Teams.GetAllTeams;
-using NhlStatsCrm.Application.Features.Teams.GetAllTeamsByGuid;
 using NhlStatsCrm.Application.Features.Teams.GetAllTeamsByAltKey;
 using NhlStatsCrm.Application.Features.Teams.UpsertTeam;
 
@@ -25,15 +24,6 @@ namespace WebAPI.Controllers
 		public async Task<IActionResult> GetAllTeams ()
 		{
 			var query = new GetAllTeamsQuery();
-			var result = await _mediator.Send(query);
-
-			return Ok(result);
-		}
-
-		[HttpGet("{teamGuid:guid}")]
-		public async Task<IActionResult> GetTeamByGuid (Guid teamGuid)
-		{
-			var query = new GetAllTeamsByTeamGuidQuery(teamGuid);
 			var result = await _mediator.Send(query);
 
 			return Ok(result);
