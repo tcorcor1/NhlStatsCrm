3bfea8d3230c01ca0c64fa52128a68a31d9b726a
Ty Corcoran <45568306+tcorcor1@users.noreply.github.com>
2022-08-22 21:33:18 -0400

    Domain: fix Crm Player model

--- src/Domain/Entities/Crm/Player.cs
+++ src/Domain/Entities/Crm/Player.cs
@@ -1,24 +1,28 @@
-﻿namespace NhlStatsCrm.Domain.Entities.Crm
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NhlStatsCrm.Domain.Entities.Crm
 {
 	public class Player
 	{
-		public Guid PlayerId { get; set; }
-		public Guid? TeamId { get; set; }
+		[Required]
 		public string? LegacyId { get; set; }
+
+		[Required]
 		public string? FullName { get; set; }
-		public string? BirthCountry { get; set; }
-		public int? BirthDate { get; set; }
+
+		[Required]
+		public string? TeamId { get; set; }
+
+		[Required]
 		public string? Link { get; set; }
-		public string? PositionName { get; set; }
-		public string? PositionType { get; set; }
+
+		[Required]
 		public int? JerseyNumber { get; set; }
-		public int? Weight { get; set; }
-		public string? Height { get; set; }
-		public decimal? AvgGoals { get; set; }
-		public decimal? AvgAssists { get; set; }
-		public decimal? AvgPoints { get; set; }
-		public decimal? AvgHits { get; set; }
-		public decimal? AvgPim { get; set; }
-		public decimal? AvgShots { get; set; }
+
+		[Required]
+		public string? PositionName { get; set; }
+
+		[Required]
+		public string? PositionType { get; set; }
 	}
 }
