7c60407a4fa543bc255824c8625b0fd6ece7bd52
Ty Corcoran <45568306+tcorcor1@users.noreply.github.com>
2022-08-23 20:53:49 -0400

    Application: add players feature

--- src/Application/Features/Players/GetAllPlayers/GetAllPlayersHandler.cs
+++ src/Application/Features/Players/GetAllPlayers/GetAllPlayersHandler.cs
@@ -1,27 +1,20 @@
-﻿//using NhlStatsCrm.Application.Interfaces.Repositories;
-//using NhlStatsCrm.Domain.Entities.Crm;
+﻿using NhlStatsCrm.Application.Interfaces.Repositories;
+using NhlStatsCrm.Domain.Entities.Crm;
 
-//namespace NhlStatsCrm.Application.Features.Players.GetAllPlayers
-//{
-//	public class GetAllPlayersHandler : IRequestHandler<GetAllPlayersQuery, IEnumerable<Player?>>
-//	{
-//		private IMapper _mapper;
-//		private IDynamicsRepository<Player> _playersRepository;
+namespace NhlStatsCrm.Application.Features.Players.GetAllPlayers
+{
+	public class GetAllPlayersHandler : IRequestHandler<GetAllPlayersQuery, IEnumerable<Player?>>
+	{
+		private IDynamicsRepository<Player> _playersRepository;
 
-//		public GetAllPlayersHandler (IDynamicsRepository<Player> playersRepository, IMapper mapper)
-//		{
-//			_mapper = mapper;
-//			_playersRepository = playersRepository;
-//		}
+		public GetAllPlayersHandler (IDynamicsRepository<Player> playersRepository)
+		{
+			_playersRepository = playersRepository;
+		}
 
-//		public async Task<IEnumerable<Player?>> Handle (GetAllPlayersQuery request, CancellationToken cancellationToken)
-//		{
-//			var playersEtn = await _playersRepository.GetAllAsync();
-
-//			return playersEtn.Select(p =>
-//			{
-//				return _mapper.Map<Player>(p.Attributes.ToDictionary(e => e.Key, e => e.Value));
-//			});
-//		}
-//	}
-//}
+		public async Task<IEnumerable<Player?>> Handle (GetAllPlayersQuery request, CancellationToken cancellationToken)
+		{
+			return await _playersRepository.GetAllAsync();
+		}
+	}
+}
--- src/Application/Features/Players/GetAllPlayersByAltKey/GetAllPlayersByPlayerAltKeyHandler.cs
+++ src/Application/Features/Players/GetAllPlayersByAltKey/GetAllPlayersByPlayerAltKeyHandler.cs
@@ -1,26 +1,20 @@
-﻿//using NhlStatsCrm.Application.Interfaces.Repositories;
-//using NhlStatsCrm.Domain.Entities.Crm;
+﻿using NhlStatsCrm.Application.Interfaces.Repositories;
+using NhlStatsCrm.Domain.Entities.Crm;
 
-//namespace NhlStatsCrm.Application.Features.Players.GetAllPlayersByAltKey
-//{
-//	public class GetAllPlayersByPlayerAltKeyHandler : IRequestHandler<GetAllPlayersByPlayerAltKeyQuery, Player>
-//	{
-//		private IMapper _mapper;
-//		private IDynamicsRepository<Player> _playersRepository;
+namespace NhlStatsCrm.Application.Features.Players.GetAllPlayersByAltKey
+{
+	public class GetAllPlayersByPlayerAltKeyHandler : IRequestHandler<GetAllPlayersByPlayerAltKeyQuery, Player?>
+	{
+		private IDynamicsRepository<Player> _playersRepository;
 
-//		public GetAllPlayersByPlayerAltKeyHandler (IDynamicsRepository<Player> playersRepository, IMapper mapper)
-//		{
-//			_mapper = mapper;
-//			_playersRepository = playersRepository;
-//		}
+		public GetAllPlayersByPlayerAltKeyHandler (IDynamicsRepository<Player> playersRepository)
+		{
+			_playersRepository = playersRepository;
+		}
 
-//		public async Task<Player> Handle (GetAllPlayersByPlayerAltKeyQuery request, CancellationToken cancellationToken)
-//		{
-//			var playerEtn = await _playersRepository.GetByAltKeyAsync(request.PlayerId);
-
-//			var playerDic = playerEtn.Attributes.ToDictionary(pair => pair.Key, pair => pair.Value);
-
-//			return _mapper.Map<Player>(playerDic);
-//		}
-//	}
-//}
+		public async Task<Player?> Handle (GetAllPlayersByPlayerAltKeyQuery request, CancellationToken cancellationToken)
+		{
+			return await _playersRepository.GetByAltKeyAsync(request.PlayerId);
+		}
+	}
+}
